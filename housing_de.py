3bf0d3f179c45da5f439a0097c69e8544568fe5d
barto-ber <b.szojda@gmail.com>
2020-03-25 20:27:16 +0100

    v2

--- housing_de.py
+++ housing_de.py
@@ -17,12 +17,13 @@ def read_housing():
     data = pd.read_csv("immo_data.csv", dtype=dtypes)
     return data
 
+
 def drop_columns():
     data = read_housing()
     less_columns = ["telekomTvOffer", "serviceCharge", "telekomHybridUploadSpeed", "newlyConst", "picturecount", "pricetrend",
                     "geo_bln", "yearConstructedRange", "interiorQual", "street", "baseRentRange", "thermalChar", "regio2",
                     "description", "facilities", "energyEfficiencyClass", "electricityBasePrice", "electricityKwhPrice",
-                    "telekomUploadSpeed", "noParkSpaces", "garden",
+                    "telekomUploadSpeed", "noParkSpaces", "garden", "livingSpaceRange", "heatingCosts", "noRoomsRange",
                     "cellar", "lift"]
     data.drop(less_columns, inplace=True, axis=1)
     data = data[data['totalRent'] < 3000]
@@ -30,7 +31,6 @@ def drop_columns():
     data = data[data['noRooms'] < 8]
     data = data[data['floor'] <= 20]
     data = data[data['numberOfFloors'] <= 20]
-    data = data[data['heatingCosts'] <= 500]
     data = data[data['livingSpace'] <= 500]
     data = data[data['yearConstructed'] <= 2020]
     data = data[data['yearConstructed'] >= 1850]
@@ -54,18 +54,19 @@ def data_geo_num():
     print("\nCheck Values:\n", data['regio1'].unique())
     return data
 
+
 def data_median_rent():
-    data = data_geo_num()
-    m = data.groupby('regio1')['baseRent']
-    data['median_base_rent'] = m.transform(np.median)
-    data['rooms_per_livingspace'] = data['noRooms'] / data['livingSpace']
-    print("\nMedian base rent in Lands:\n", data.head())
-    return data
+    data_with_median_rent = data_geo_num()
+    m = data_with_median_rent.groupby('regio1')['baseRent']
+    data_with_median_rent['median_base_rent'] = m.transform(np.median)
+    data_with_median_rent['rooms_per_livingspace'] = data_with_median_rent['noRooms'] / data_with_median_rent['livingSpace']
+    print("\nMedian base rent in Lands:\n", data_with_median_rent.head())
+    return data_with_median_rent
 
 
 def check_data():
-    data = drop_columns()
-    # print(data.head())
+    data = data_median_rent()
+    print(data[:50])
     print(data.info())
     # print(data.describe())
     # print("\nLets see PLZ:\n", data[
@@ -97,7 +98,8 @@ def plz_hist():
     data['regio1_category'].hist()
     plt.show()
 
-def land_scater():
+
+def land_scatter():
     data = data_median_rent()
     data.plot(kind="scatter", x="regio1", y='median_base_rent')
     plt.show()
@@ -107,22 +109,30 @@ def land_scater():
 from sklearn.model_selection import StratifiedShuffleSplit
 
 def create_test_set():
-    data = data_geo_num()
+    data = data_median_rent()
     split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
     for train_index, test_index in split.split(data, data["regio1"]):
         strat_train_set = data.loc[train_index]
         strat_test_set = data.loc[test_index]
-
         check_test = strat_test_set["regio1"].value_counts() / len(strat_test_set)
         print(check_test)
+        # print("\nCheck strat train:\n", strat_train_set.info()) # Result NONE, if checking whole dtrat training mostly NANs. WHY???
         return strat_train_set, strat_test_set
+create_test_set()
+
+def data_labels():
+    data, strat_train_set = create_test_set()
+    data = strat_train_set.drop("median_base_rent", axis=1)
+    data_labels = strat_train_set["median_base_rent"].copy()
+    print("\nData labels info:\n", data_labels)
+    return data, data_labels
 
 
 def looking_for_correlations():
     data = data_median_rent()
     corr_matrix = data.corr()
     print("\nCorrelations:\n", corr_matrix["baseRent"].sort_values(ascending=False))
-looking_for_correlations()
+
 
 from pandas.plotting import scatter_matrix
 def plot_correlations():
@@ -136,3 +146,12 @@ def most_promising_corr_scater():
     data = data_median_rent()
     data.plot(kind="scatter", x="lastRefurbish", y='baseRent', alpha=0.1)
     plt.show()
+
+
+
+
+
+
+
+
+
