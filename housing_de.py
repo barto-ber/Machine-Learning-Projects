97e1a483fe14ba793c7a4822bed6a8cc2bd315fa
barto-ber <b.szojda@gmail.com>
2020-03-27 10:15:12 +0100

    lin regr2

--- housing_de.py
+++ housing_de.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 pd.options.display.width = 0
 pd.options.display.max_rows = None
-pd.options.display.precision = 1
+pd.options.display.precision = 5
 
 
 def read_housing():
@@ -25,7 +25,7 @@ def drop_columns():
                     "description", "facilities", "energyEfficiencyClass", "electricityBasePrice", "electricityKwhPrice",
                     "telekomUploadSpeed", "noParkSpaces", "garden", "livingSpaceRange", "heatingCosts", "noRoomsRange",
                     "cellar", "lift", "condition", "heatingType", "firingTypes", "geo_krs", "streetPlain", "petsAllowed",
-                    "typeOfFlat", "regio3", "houseNumber", "date"]
+                    "typeOfFlat", "regio3", "houseNumber", "date", "scoutId", "geo_plz", "balcony", "hasKitchen"]
     data.drop(less_columns, inplace=True, axis=1)
     data = data[data['totalRent'] < 3000]
     data = data[data['baseRent'] < 3000]
@@ -61,14 +61,14 @@ def data_median_rent():
     m = data_with_median_rent.groupby('regio1')['baseRent']
     data_with_median_rent['median_base_rent'] = m.transform(np.median)
     data_with_median_rent['rooms_per_livingspace'] = data_with_median_rent['noRooms'] / data_with_median_rent['livingSpace']
-    print("\nMedian base rent in Lands:\n", data_with_median_rent.head())
+    # print("\nMedian base rent in Lands:\n", data_with_median_rent.head())
     return data_with_median_rent
 
 
 def check_data():
     data = data_median_rent()
-    print(data[:50])
-    print(data.info())
+    # print(data[:50])
+    # print(data.info())
     # print(data.describe())
     # print("\nLets see PLZ:\n", data[
     #                                 (data['geo_plz'] <= 10000)].head())
@@ -123,12 +123,22 @@ def create_test_set():
         return strat_train_set, strat_test_set
 
 
-def data_labels():
+def data_for_labels():
     data, strat_train_set = create_test_set()
-    data = strat_train_set.drop("median_base_rent", axis=1)
+    data_prepared = strat_train_set.drop("median_base_rent", axis=1)
     data_labels = strat_train_set["median_base_rent"].copy()
-    print("\nData labels info:\n", data_labels)
-    return data, data_labels
+    # print("\nData labels info:\n", data_labels)
+    return data_prepared, data_labels
+
+
+from sklearn.impute import SimpleImputer
+def replace_nan_train_set():
+    data_prepared, data_labels = data_for_labels()
+    # print(data_prepared)
+    data_prepared.fillna(data_prepared.median(), inplace=True)
+    # print("\nHow many NaN in dataset?\n", data_prepared.isnull().sum().sum())
+    # print(data_prepared[:50])
+    return data_prepared, data_labels
 
 
 def looking_for_correlations():
@@ -150,13 +160,30 @@ def most_promising_corr_scater():
     data.plot(kind="scatter", x="lastRefurbish", y='baseRent', alpha=0.1)
     plt.show()
 
+# Transformation Pipelines
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+data_prepared, data_labels = replace_nan_train_set()
+print("\nHow many INF in dataset?\n", data_prepared.isin([np.inf, -np.inf]).sum().sum())
+# num_pipeline = Pipeline([
+#                         # ('imputer', SimpleImputer(strategy="median")),
+#                         # ('attribs_adder', CombinedAttributesAdder()),
+#                         ('std_scaler', StandardScaler())
+#                             ])
+# data_prepared_transformed = num_pipeline.fit_transform(data_prepared)
+
 
 # Training and Evaluating on the Training Set
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(housing_prepared, housing_labels)
-
-
+# from sklearn.linear_model import LinearRegression
+#
+# lin_reg = LinearRegression()
+# lin_reg.fit(data_prepared, data_labels)
+#
+# some_data = data_prepared_transformed.iloc[:5]
+# some_labels = data_labels.iloc[:5]
+# print("\nPredictions:\n", lin_reg.predict(some_data))
+# print("\nLabels:\n", list(some_labels))
 
 
 
