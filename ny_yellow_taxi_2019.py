ea9e24545343b80f2a85d8d74c6c6c99237f38d3
barto-ber <b.szojda@gmail.com>
2020-05-06 21:40:03 +0200

    checked data one more time, still some infinity

--- ny_yellow_taxi_2019.py
+++ ny_yellow_taxi_2019.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
 pd.options.display.width = 0
 pd.options.display.max_rows = None
+pd.options.display.float_format = "{:.2f}".format
 
 '''Reading data from CSV'''
 print("--- Reading data ---\n")
@@ -27,26 +28,19 @@ data = pd.read_csv('yellow_tripdata_2019-01.csv')
 data = data[['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'trip_distance', 'RatecodeID', 'PULocationID', 'DOLocationID',
              'payment_type', 'fare_amount']]
 
-print(data.head())
-print("Max fare amount:\n", data['fare_amount'].max())
-print("Min fare amount:\n", data['fare_amount'].min())
+# print(data.head())
 # print(data.describe())
 
 '''CHECK FOR NaNs (generally and in detail)'''
-# with ProgressBar():
-#     missing_values_all = data.isnull().sum().sum().compute()
+# pd.set_option('use_inf_as_na', True)
+# data = data.replace([np.inf, -np.inf], 0).dropna(subset=data.columns, how="all")
+#
+# missing_values_all = data.isnull().sum().sum()
 # print("\nHow many NaNs in dataset?\n", missing_values_all)
 #
 # missing_values = data.isnull().sum()
 # missing_count = (missing_values / data.index.size * 100)
-# with ProgressBar():
-#     missing_count_pct = missing_count.compute()
-# print("\nWhere are the NaNs:\n", missing_count_pct)
-
-'''How many rows in dataset'''
-# with ProgressBar():
-#     rows_sum = data.index.size.compute()
-# print("So many rows in dataset:\n", rows_sum)
+# print("\nWhere are the NaNs:\n", missing_count)
 
 '''Throwing out unknown zones 264 & 265 and zone 1 (Newark Airport)'''
 print("--- Throwing out bad data points ---\n")
@@ -56,11 +50,15 @@ data = data[data['PULocationID'] < 264]
 data = data[data['DOLocationID'] < 264]
 data = data[data['RatecodeID'] == 1]
 data = data[data['fare_amount'] > 0]
-data = data[data['trip_distance'] > 0]
 data['trip_distance'] = (data['trip_distance'] * 1.609344).round(decimals=2) # getting KM
-print(data.describe())
-print("Max fare amount:\n", data['fare_amount'].max())
-print("Min fare amount:\n", data['fare_amount'].min())
+data = data[data['trip_distance'] >= 0.5]
+data = data[data['trip_distance'] < 100]
+data = data[data['fare_amount'] < 150]
+data = (data[data['fare_amount'] >= 2.5])
+data = data[data['payment_type'] <= 2]
+
+# print(data.describe())
+
 '''Converting to datetime'''
 print("--- Converting to datetime ---\n")
 data['tpep_pickup_datetime'] = pd.to_datetime(data['tpep_pickup_datetime'])
@@ -80,18 +78,6 @@ data['trip_time'] = data['tpep_dropoff_datetime'] - data['tpep_pickup_datetime']
 #     min_date = data['tpep_pickup_datetime'].min().compute()
 # print(min_date)
 
-'''Check for unknown zones etc.'''
-# unknown_zones = (data['PULocationID'] >= 264).sum()
-# standard_rate = (data['RatecodeID'] > 1).sum()
-# no_charge = (data['payment_type'] >= 3).sum()
-# zero_charge = (data['fare_amount'] == 3).sum()
-# night_charge = data[(data['trip_distance'] <= 2)]
-# print("\nHow many unknown zones in dataset?\n", unknown_zones)
-# print("\nHow many not standard trip rates in dataset?\n", standard_rate)
-# print("\nHow many not charged etc. in dataset?\n", no_charge)
-# print("\nHow many with zero-close charges in dataset?\n", zero_charge)
-# print("\nChecking night charge:\n", night_charge[500000:500050])
-
 '''Converting datetime to unix'''
 print("--- Converting datetime to unix time ---\n")
 data_unix = data.copy()
@@ -100,13 +86,12 @@ data_unix['tpep_dropoff_datetime'] = (data_unix['tpep_dropoff_datetime'] - pd.Ti
 
 print("--- Adding additional attribute combinations ---\n")
 data_unix['trip_time'] = data_unix['tpep_dropoff_datetime'] - data_unix['tpep_pickup_datetime']
-# data['time/distance'] = (data['trip_time_nix'] / data['trip_distance']).round(decimals=2) # lin correlations close zero
-# data['distance/time'] = (data['trip_distance'] / data['trip_time_nix']).round(decimals=5) # lin correlations close zero
+data_unix['distance/time'] = data_unix['trip_distance'] / data_unix['trip_time'] # lin correlations close zero
 print("Unix time data head:\n", data_unix.head())
 
 '''Creating train and test set'''
-# print("--- Creating train and test set ---\n")
-# train_set, test_set = train_test_split(data_unix, test_size=0.3, random_state=42)
+print("--- Creating train and test set ---\n")
+train_set, test_set = train_test_split(data_unix, test_size=0.3, random_state=42)
 
 '''Looking for correlations'''
 # print("--- Checking for linear pearsons correlations ---\n")
@@ -121,13 +106,12 @@ data_unix.drop(irrelevant_features, inplace=True, axis=1)
 
 '''Split data'''
 print("--- Splitting data ---\n")
+
 predict = 'fare_amount'
-print(data_unix.head())
-print(data_unix.describe())
+
 X = data_unix.drop([predict], axis=1)
 y = data_unix[predict]
-print(X.head())
-print(X.describe())
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 '''Algorithms'''
@@ -145,7 +129,7 @@ scale_transform = ColumnTransformer([
                             ('cat', OneHotEncoder(), cat_attribs_X_train)
                         ])
 
-# X_train_prepared = scale_transform.fit_transform(X_train)
+X_train_prepared = scale_transform.fit_transform(X_train)
 # X_test_prepared = scale_transform.fit_transform(X_test)
 
 def display_scores(scores):
@@ -155,10 +139,10 @@ def display_scores(scores):
 
 lin_reg = LinearRegression()
 
-# print("\n\tCross validation for Linear Regression:")
-# lin_reg_scores = cross_val_score(lin_reg, X_train_prepared, y_train, scoring="neg_mean_squared_error", cv=10, n_jobs=-1)
-# lin_reg_rmse_scores = np.sqrt(-lin_reg_scores)
-# display_scores(lin_reg_rmse_scores)
+print("\n\tCross validation for Linear Regression:")
+lin_reg_scores = cross_val_score(lin_reg, X_train_prepared, y_train, scoring="neg_mean_squared_error", cv=10, n_jobs=-1)
+lin_reg_rmse_scores = np.sqrt(-lin_reg_scores)
+display_scores(lin_reg_rmse_scores)
 
 # lin_reg.fit(X_train_prepared, y_train)
 # predictions = lin_reg.predict(X_test_prepared)
