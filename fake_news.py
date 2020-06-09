d725704bb977f69b19ff500f88225dffd7abadc8
barto-ber <b.szojda@gmail.com>
2020-06-09 13:07:19 +0200

    The task is to classify the article in two groups politicsNews and worldNews.
    PART 1 - CountVectorizer, TF-IDF & Log Reg
    Conclusion: First try was to apply Bag of words with default settings as well as Logistic Regression with default settings.
    The cross validation accuracy was 0.935, not bad. Using GridSearchCV and finding best params was possible to push
    the cross validation accuracy to 0.942. Testing this best model on the test set got accuracy of 0.936, not bad.
    
    Trying to get more using Stop-words in Countvectorizer got the cross validation accuracy of 0.942 same as without Stop words.
    Last try for rescaling features with TF-IDF with different n_grams-values got the cross validation of 0.946,
    kind of the same as other methods.
    
    PART 2 - Latent Dirichlet Allocation (LDA)
    Conclusion: This approach was quite successful; it seems that the algorithm recognized two groups politics news amd world news.
    We can see it in the print of some topic words.
    
    PRINT OF ARTICLES NOT POSSIBLE?????

--- fake_news.py
+++ fake_news.py
@@ -2,9 +2,7 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import re
-from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict, GridSearchCV
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
@@ -12,17 +10,36 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import spacy
 import nltk
 from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.metrics import confusion_matrix, plot_confusion_matrix, \
     precision_recall_curve, roc_curve, precision_score, recall_score, \
     classification_report, roc_auc_score
 import textwrap
 import matplotlib.pyplot as plt
+import mglearn
 pd.options.display.width = 0
 pd.options.display.max_rows = None
 pd.options.display.float_format = "{:.2f}".format
 
 
-'''The task is to classify the article in two groups politicsNews and worldNews'''
+'''The task is to classify the article in two groups politicsNews and worldNews.
+
+PART 1 - CountVectorizer, TF-IDF & Log Reg
+Conclusion: First try was to apply Bag of words with default settings as well as Logistic Regression with default settings.
+The cross validation accuracy was 0.935, not bad. Using GridSearchCV and finding best params was possible to push 
+the cross validation accuracy to 0.942. Testing this best model on the test set got accuracy of 0.936, not bad.
+
+Trying to get more using Stop-words in Countvectorizer got the cross validation accuracy of 0.942 same as without Stop words.
+Last try for rescaling features with TF-IDF with different n_grams-values got the cross validation of 0.946, 
+kind of the same as other methods.
+
+
+PART 2 - Latent Dirichlet Allocation (LDA)
+Conclusion: This approach was quite successful; it seems that the algorithm recognized two groups politics news amd world news.
+We can see it in the print of some topic words.
+
+PRINT OF ARTICLES NOT POSSIBLE?????
+'''
 
 time_now = datetime.now()
 
@@ -49,7 +66,7 @@ print("\nUnique categories of Subject\n", data['subject'].unique())
 # missing_count = (missing_values / data.index.size * 100)
 # print("\nWhere are the NaNs:\n", missing_count)
 
-# '''regex'''
+'''regex to clean up the first words of each article which are the name of the source'''
 # reuters_regex = re.compile(r'((\D+)(\(\D*\)))')
 # for i in data['text']:
 #     check_reuters = reuters_regex.search(i)
@@ -73,7 +90,11 @@ y = data[predict].copy()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-'''Applying Bag-of-Words'''
+
+
+'''
+---PART 1
+Applying Bag-of-Words'''
 # print("\n--- Applying Bag-of-Words ---\n")
 # vect = CountVectorizer()
 # X_train = vect.fit_transform(X_train.text)
@@ -90,20 +111,20 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 # lr = LogisticRegression(max_iter=1000000)
 # scores_lr = cross_val_score(lr, X_train, y_train, cv=10)
 # print("Mean cross-validation accuracy:\n", np.mean(scores_lr))
-'''Mean cross-validation accuracy: 0.9350017939881635'''
+'''===Mean cross-validation accuracy: 0.9350017939881635'''
 
 # param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
 # grid = GridSearchCV(lr, param_grid, cv=10)
 # grid.fit(X_train, y_train)
 # print("Best cross-validation score:\n", grid.best_score_)
 # print("Best parameters:\n ", grid.best_params_)
-'''Best cross-validation score: 0.9427222588597054
+'''===Best cross-validation score: 0.9427222588597054
 C=0,01'''
 
 # lr = LogisticRegression(max_iter=1000000, C=0.01)
 # X_test = vect.transform(X_test.text)
 # print("Score on X_test", grid.score(X_test, y_test))
-'''Score on X_test 0.9366946778711485'''
+'''===Score on X_test 0.9366946778711485'''
 
 
 '''Applying Bag-of-Words with STOPWORDS'''
@@ -115,7 +136,7 @@ C=0,01'''
 # grid = GridSearchCV(lr, param_grid, cv=10)
 # grid.fit(X_train, y_train)
 # print("Best cross-validation score with Stopwords:\n", grid.best_score_)
-'''Best cross-validation score with Stopwords: 0.9429712473681068'''
+'''===Best cross-validation score with Stopwords: 0.9429712473681068'''
 
 
 '''Model with rescaling with tf-idf'''
@@ -127,37 +148,37 @@ C=0,01'''
 # print("Best parameters tf-idf:\n ", grid.best_params_)
 # print("Best cross-validation score with tf-idf:\n", grid.best_score_)
 '''
--min_df=0.5
+===min_df=0.5
 Best parameters tf-idf:
   {'logisticregression__C': 0.001}
 Best cross-validation score with tf-idf:
  0.7029631802334277
  
--min_df=0.2
+===min_df=0.2
 Best parameters tf-idf:
   {'logisticregression__C': 0.01}
 Best cross-validation score with tf-idf:
  0.8973980584630908
 
--min_df=0.1
+===min_df=0.1
 Best parameters tf-idf:
   {'logisticregression__C': 0.001}
 Best cross-validation score with tf-idf:
  0.923982405742002
 
--min_df=0.05
+===min_df=0.05
 Best parameters tf-idf:
   {'logisticregression__C': 0.001}
 Best cross-validation score with tf-idf:
  0.9325742528988602
  
- -min_df=0.001
+===min_df=0.001
  Best parameters tf-idf:
   {'logisticregression__C': 0.001}
 Best cross-validation score with tf-idf:
  0.9458353514085713
  
- min_df=0.00025
+===min_df=0.00025
  Best parameters tf-idf:
   {'logisticregression__C': 0.001}
 Best cross-validation score with tf-idf:
@@ -177,14 +198,15 @@ Best cross-validation score with tf-idf:
 # print("Best parameters with n-grams:\n", grid.best_params_)
 # print("Best cross-validation score with n-grams:\n", grid.best_score_)
 '''
-Best parameters with n-grams:
+===Best parameters with n-grams:
  {'logisticregression__C': 10, 'tfidfvectorizer__ngram_range': (1, 1)}
-Best cross-validation score with n-grams:
+===Best cross-validation score with n-grams:
  0.9468310729599099
 '''
 
-'''Best model CHECK'''
-# print("\n--- Computing Best Model ---\n")
+
+'''First best model CHECK'''
+# print("\n--- Computing First best model ---\n")
 # lr_best = LogisticRegression(max_iter=1000000, C=0.01)
 # lr_best.fit(X_train, y_train)
 #
@@ -192,14 +214,14 @@ Best cross-validation score with n-grams:
 # c_matr = confusion_matrix(y_train, predictions_lr_best)
 # print("Confusion matrix:\n", c_matr)
 '''
-Confusion matrix:
+===Confusion matrix:
  [[7930  525]
  [ 395 7212]]
 '''
 
 
-'''Search grid CountVectorizer plus Log reg'''
-# print("\n--- Computing Search grid COuntVectorizer plus Log reg ---\n")
+'''Looking for better model Search grid CountVectorizer plus Log reg'''
+# print("\n--- Computing Search grid CountVectorizer plus Log reg ---\n")
 # pipe = Pipeline([('vect', CountVectorizer()),
 #                  ('lr', LogisticRegression(max_iter=1000000))
 #                  ])
@@ -212,38 +234,37 @@ Confusion matrix:
 # print("Best parameters with search grid Vect and Lr:\n", grid.best_params_)
 # print("Best cross-validation score with search grid Vect and Lr:\n", grid.best_score_)
 '''
-Best parameters with search grid Vect and Lr:
+===Best parameters with search grid Vect and Lr:
  {'lr__C': 0.01, 'vect__max_df': 0.75}
-Best cross-validation score with search grid Vect and Lr:
+===Best cross-validation score with search grid Vect and Lr:
  0.9430958578634414
 '''
+'''Using best params for Countvect for best model and CHECK'''
+# print("\n--- Best params - Applying Bag-of-Words ---\n")
+# vect = CountVectorizer(max_df=0.75)
+# X_train = vect.fit_transform(X_train.text)
+# X_test = vect.transform((X_test.text))
+#
+# print("\n--- Computing Best Model ---\n")
 
-print("\n--- Applying Bag-of-Words ---\n")
-vect = CountVectorizer(max_df=0.75)
-X_train = vect.fit_transform(X_train.text)
-X_test = vect.transform((X_test.text))
-
-print("\n--- Computing Best Model ---\n")
-lr_best = LogisticRegression(max_iter=1000000, C=0.01, n_jobs=-1, random_state=42)
-lr_best.fit(X_train, y_train)
+# lr_best.fit(X_train, y_train)
 
 # predictions_lr_best = cross_val_predict(lr_best, X_train, y_train,
 #                                         cv=5, n_jobs=-1, method='decision_function')
 # c_matr = confusion_matrix(y_train, predictions_lr_best)
 # print("Confusion matrix:\n", c_matr)
-
 '''
 Confusion matrix:
  [[7936  519]
  [ 395 7212]]
 '''
-
 # plot_confusion_matrix(lr_best, X_train, y_train)
 # plt.show()
 
-'''TESTING ON THE TEST DATA'''
-print("\n--- Testing Best Model ---\n")
-predictions_lr_best_test = lr_best.predict(X_test)
+
+'''TESTING best model ON THE TEST DATA'''
+# print("\n--- Testing Best Model ---\n")
+# predictions_lr_best_test = lr_best.predict(X_test)
 # c_matr = confusion_matrix(y_test, predictions_lr_best_test)
 # print("Test best model confusion matrix:\n", c_matr)
 # plot_confusion_matrix(lr_best, X_test, y_test, values_format='.2f')
@@ -252,7 +273,6 @@ Test best model confusion matrix:
  [[2638  179]
  [ 152 2386]]
 '''
-
 
 # classsif_rep_lr_best_test = classification_report(y_test, predictions_lr_best_test)
 # print("Classification report test best model:\n", classsif_rep_lr_best_test)
@@ -267,8 +287,8 @@ Test best model confusion matrix:
 weighted avg       0.94      0.94      0.94      5355
 '''
 
-auc = roc_auc_score(y_test, lr_best.decision_function(X_test))
-print("AUC of test best model:\n", auc)
+# auc = roc_auc_score(y_test, lr_best.decision_function(X_test))
+# print("AUC of test best model:\n", auc)
 '''
 AUC of test best model:
  0.9803684737464449
@@ -298,6 +318,65 @@ AUC of test best model:
 # plot_roc_curve(fpr, tpr)
 #
 # plt.show()
+
+
+
+'''
+---PART 2
+Latent Dirichlet Allocation (LDA)'''
+print("--- Computing Latent Dirichlet Allocation (LDA) ---")
+vect_lda = CountVectorizer(max_features=10000, max_df=0.25)
+X_lda = vect_lda.fit_transform(X_train.text)
+
+lda = LatentDirichletAllocation(n_components=2, learning_method="batch",
+                                max_iter=25, random_state=42, n_jobs=-1)
+# We build the model and transform the data in one step
+# Computing transform takes some time,
+# and we can save time by doing both at once
+document_topics = lda.fit_transform(X_lda)
+
+print("LDA shape of components:\n", lda.components_.shape)
+
+# For each topic (a row in the components_), sort the features (ascending)
+# Invert rows with [:, ::-1] to make sorting descending
+sorting = np.argsort(lda.components_, axis=1)[:, ::-1]
+# Get the feature names from the vectorizer
+feature_names = np.array(vect_lda.get_feature_names())
+# Print out the 10 topics:
+mglearn.tools.print_topics(topics=range(2), feature_names=feature_names,
+                           sorting=sorting, topics_per_chunk=5, n_words=20)
+'''
+topic 0       topic 1       
+--------      --------      
+north         campaign      
+minister      clinton       
+china         senate        
+military      obama         
+korea         percent       
+security      white         
+party         tax           
+foreign       democratic    
+iran          court         
+police        presidential  
+nuclear       she           
+eu            bill          
+russia        law           
+officials     republicans   
+war           administration
+countries     her           
+south         federal       
+european      party         
+prime         committee     
+she           congress 
+'''
+
+# sort by weight of "world news" topic 0
+music = np.argsort(document_topics[:, 0])[::-1]
+# print the five documents where the topic is most important
+for i in music[:10]:
+    # pshow first two sentences
+    print("\n".join(X_train.text[i]))#can not PRINT OUT!!!!!!
+
 
 
 
